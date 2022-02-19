ae610381f9d3a35ef53523710d83e085a87ee746
inatonix <imitation0813@gmail.com>
2022-02-19 11:44:22 +0900

    tmp

--- vue-guide/src/App.vue
+++ vue-guide/src/App.vue
@@ -1,25 +1,22 @@
-<script setup lang="ts">
-import TodoList from './components/TodoList.vue';
-import { inject, InjectionKey, provide, ref } from "vue";
-import { todos, todoKey } from './useTodo'
-
-// provide('todos', todos)
-provide(todoKey, todos)
-
-// import Reservation from './components/Reservation.vue';
-</script>
-
 <template>
-  <TodoList />
-  <!-- <Reservation></Reservation> -->
+  <Form />
 </template>
 
+<script setup lang="ts">
+import Form from './components/Form.vue';
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+  background-color: #292929;
 }
 </style>
